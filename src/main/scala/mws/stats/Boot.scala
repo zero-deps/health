2bdc14ab646335843a6906880b481864e38c1264
Andrey Nemchenko <andrey.nemchenko@playtech.com>
2015-02-05 13:06:45 +0200

    merge handler with listener

--- src/main/scala/mws/stats/Boot.scala
+++ src/main/scala/mws/stats/Boot.scala
@@ -9,20 +9,20 @@ object Boot extends App {
   val system = ActorSystem("stats")
   val config = system.settings.config
 
-  var listener: Option[ActorRef] = None
   var statsLeveldb: Option[DB] = None
 
   sys.addShutdownHook {
-    listener foreach (_ ! Listener.Disconnect)
     statsLeveldb foreach (db => Try(db.close()))
     system.shutdown()
     println("Bye!")
   }
 
-  statsLeveldb = Some(LeveldbKvs.open(config.getConfig("stats-kvs")))
-  val statsKvs = system.actorOf(StatsLeveldbKvs.props(statsLeveldb.get, "stats-kvs"), "stats-kvs")
-  val handler = system.actorOf(Handler.props(statsKvs), "handler")
-  listener = Some(system.actorOf(Listener.props(port = 12345, handler), "listener"))
+  val statsKvs: ActorRef = {
+    val configPath = "stats-kvs"
+    val leveldb = LeveldbKvs.open(config.getConfig(configPath))
+    statsLeveldb = Some(leveldb)
+    system.actorOf(StatsLeveldbKvs.props(leveldb, configPath), "stats-kvs")
+  }
 
-  listener foreach (_ ! Listener.Connect)
+  system.actorOf(Listener.props(port = 12345, statsKvs), "listener")
 }
--- src/main/scala/mws/stats/Listener.scala
+++ src/main/scala/mws/stats/Listener.scala
@@ -1,30 +1,36 @@
 package .stats
 
-import akka.actor.{Props, ActorRef, ActorLogging, Actor}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.io.{IO, Udp}
 import java.net.InetSocketAddress
-import akka.io.IO
-import akka.io.Udp
 
 object Listener {
-  case object Connect
-  case object Disconnect
-
-  def props(port: Int, handler: ActorRef): Props = Props(new Listener(port, handler))
+  def props(port: Int, kvs: ActorRef): Props = Props(new Listener(port, kvs))
 }
 
-class Listener(port: Int, handler: ActorRef) extends Actor with ActorLogging {
-  implicit val system = context.system
+class Listener(port: Int, kvs: ActorRef) extends Actor with ActorLogging {
+  import context.system
+  IO(Udp) ! Udp.Bind(self, new InetSocketAddress("localhost", port))
 
   def receive: Receive = {
-    case Listener.Connect =>
-      IO(Udp) ! Udp.Bind(handler, new InetSocketAddress(port))
-
     case Udp.Bound(_) =>
-      val worker = sender
-      context.become {
-        case Listener.Disconnect =>
-          worker ! Udp.Unbind
-          context.become(receive)
+      val socket = sender
+      context become (ready(sender))
+  }
+
+  def ready(socket: ActorRef): Receive = {
+    case Udp.Received(data, _) =>
+      data.decodeString("UTF-8").split('#') match {
+        case Array(node, stats, _*) =>
+          kvs ! StatsKvsService.Put(node, stats)
+          kvs ! StatsKvsService.All
+        case _ =>
       }
+    case StatsKvsService.Values(values) =>
+      log.info(values.mkString(","))
+    case Udp.Unbind =>
+      socket ! Udp.Unbind
+    case Udp.Unbound =>
+      context.stop(self)
   }
 }
