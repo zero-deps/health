5f10d7eb55fb3f13c7d63d0dc4e649a7cac298b1
Andrey Nemchenko <andrey.nemchenko@playtech.com>
2015-06-10 09:33:30 +0300

    implement last updated cell

--- src/main/resources/public/home.js
+++ src/main/resources/public/home.js
@@ -4,8 +4,10 @@ var ws = new WebSocket(wsUrl)
 ws.onmessage = function(event) {
   // data: "{name}#{node}#{param}#{time}#{value}"
   var arr = event.data.split('#');
+  //todo multidimensional array
   data[Array(arr[0], arr[1], arr[2]).join('#')] = arr[4];
-  React.render(<TabbedTable data={data} />, document.getElementById('tableContainer'));
+  data[Array(arr[0], arr[1]).join('#')] = new Date();
+  React.render(<TabbedTable data={data} />, document.getElementById("tableContainer"));
 };
 
 var TabbedTable = React.createClass({
@@ -83,19 +85,42 @@ var Table = React.createClass({
 });
 
 var Row = React.createClass({
+  getInitialState: function() {
+    return { time: new Date() };
+  },
+  componentDidMount: function() {
+    this.timer = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+  },
+  tick: function() {
+    this.setState({ time: new Date() });
+  },
   render: function() {
     var data = this.props.data;
     var name = this.props.name;
     var node = this.props.node;
-    var cells = this.props.params.map(function(param) {
+    var paramCells = this.props.params.map(function(param) {
       var value = data[Array(name, node, param).join('#')];
       return <td>{value}</td>;
     });
+
+    var lastUpdated, className;
+    var elapsed = Math.floor((this.state.time - data[Array(name, node).join('#')]) / 1000);
+    if (elapsed < 3) {
+      lastUpdated = "just now";
+      className = "success";
+    } else {
+      lastUpdated = elapsed + " seconds ago";
+      className = "danger";
+    }
+
     return (
-      <tr>
+      <tr className={className}>
         <td>{this.props.node}</td>
-        {cells}
-        <td>just now</td>
+        {paramCells}
+        <td>{lastUpdated}</td>
       </tr>
     );
   }
