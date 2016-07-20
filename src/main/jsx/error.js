3dffe9fc85992c510423afb8c6dc7c821b1e404c
Vova Polishchuk <volodymyr.polishchuk@playtech.com>
2016-07-20 13:24:30 +0300

    Misha's fixes

--- src/main/jsx/error.js
+++ src/main/jsx/error.js
@@ -12,13 +12,15 @@ var StackTrace = React.createClass({
 	
 	render: function() {
 		var stackTraces = [];
-		for(i = 1; i < this.props.errors.length; i++){
+		for(i = 0; i < this.props.errors.length; i++){
 			stackTraces[2*i] =  this.props.errors[i];
 			stackTraces[2*i+1] = React.createElement('br');
 		}
-		return (<div onClick={this.onClick} style={{cursor:"pointer"}}>
-			{this.props.errors[0]}
-			<div >
+
+		var scroll = {display: "inline-block"}
+		return (<div onClick={this.onClick} style={{cursor:"pointer",  whiteSpace: "nowrap"}}>
+			{this.state.showTrace ? "" : this.props.errors[0]}
+			<div style={scroll}>
 				{this.state.showTrace ? stackTraces : ""}
 			</div>
 		</div>);
@@ -40,26 +42,30 @@ var Errors = React.createClass({
 		for(i = 0; i < stackTraces.length; i++){
 			errors[i] = "at " + stackTraces[i].className + "." + stackTraces[i].method + " (" + stackTraces[i].fileName + ":" + stackTraces[i].lineNumber + ")";
 		}
+
 		return {name:arr[0],node:arr[1],time:arr[2],msg:arr[3], errors:errors, title: errors[0]};
 	},
-	
+
 	getInitialState: function() {
 		return {data:[]};
 	},
-	
+
 	componentDidMount: function() {
 		this.props.handlers.error = function(inStr) {
 	        if (this.isMounted())
 	          this.setState({data:update(this.state.data,{$unshift:[this.parseIn(inStr)]})});
 	      }.bind(this);
 	},
-    
+
 	render: function() {
 		var errors = this.state.data.map(function(error) {
+			var _time = new Date();
+			_time.setTime(Number(error.time) * 1000);
+
 			return <tr onClick={this.onClick}>
 				<td>{error.name}</td>
 				<td>{error.node}</td>
-				<td>{error.time}</td>
+				<td>{_time.toString()}</td>
 				<td>{error.msg}</td>
 				<td> <StackTrace errors = {error.errors}/></td>
 			</tr>;
