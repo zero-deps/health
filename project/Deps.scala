d9e5979b366a6c907ef8176f265abbba9c2d9711
Andrey Nemchenko <andrey.nemchenko@playtech.com>
2016-10-19 15:43:36 +0300

    update scala

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,35 +5,34 @@ import sbt.Keys._
 
 object Deps {
   object Versions {
-    val scala = "2.11.8"
-    val akka = "2.4.10-12-g84649db"
+    val scala = "2.12.0-RC2"
+    val akka = "2.4.11-19-g8520080"
     val logback = "1.1.7"
-    val ftier = "0-231-g1c4cdb5"
+    val ftier = "0-236-g8a3356f"
     val argonaut = "6.2-M3"
     val sigarLoader = "1.6.6-rev002"
     val jul2slf4j = "1.7.21"
     val scalatest = "3.0.0"
   }
 
-  val stats = libraryDependencies ++= Seq(
-    "com.." %% "ftier-ws" % Versions.ftier,
-    "io.argonaut" %% "argonaut" % Versions.argonaut,
-    ("com...akka" %% "akka-slf4j" % Versions.akka).
-      exclude("org.slf4j", "slf4j-api"),
-    ("ch.qos.logback" % "logback-classic" % Versions.logback).
-      exclude("org.slf4j", "slf4j-api")
-  ) ++ test
+  val argonaut = "io.argonaut" %% "argonaut" % Versions.argonaut cross CrossVersion.fullMapped {
+    case "2.12.0-RC2" => "2.11"
+    case x => x
+  }
 
-  val stats_client = Seq("io.argonaut" %% "argonaut" % Versions.argonaut,
+  val stats_client = Seq(
+    argonaut % Provided,
     "io.kamon" % "sigar-loader" % Versions.sigarLoader,
-    "com...akka" %% "akka-slf4j" % Versions.akka,
-    ("ch.qos.logback" % "logback-classic" % Versions.logback).
-      exclude("org.slf4j", "slf4j-api"),
-    "com...akka" %% "akka-cluster" % Versions.akka,
-    "ch.qos.logback" % "logback-classic" % Versions.logback,
-    "org.slf4j" % "jul-to-slf4j" % Versions.jul2slf4j // for sigar loader
+    "org.slf4j" % "jul-to-slf4j" % Versions.jul2slf4j, // for sigar loader
+    "com...akka" %% "akka-cluster" % Versions.akka % Provided,
+    "com...akka" %% "akka-slf4j" % Versions.akka % Provided,
+    ("ch.qos.logback" % "logback-classic" % Versions.logback % Provided).exclude("org.slf4j", "slf4j-api")
   )
 
+  val stats = libraryDependencies ++= Seq(
+    "com.." %% "ftier-ws" % Versions.ftier,
+    argonaut
+  ) ++ test
 
   val test = Seq(
     "org.scalatest" %% "scalatest" % Versions.scalatest,
