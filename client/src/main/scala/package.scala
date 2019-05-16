0801d1f60e0338daa656923a9b4adb6c62d88e25
Yuriy Mazepin <yuriy.mazepin@playtech.com>
2019-05-16 19:02:43 +0300

    Using proto for UDP messages from cms

--- client/src/main/scala/package.scala
+++ client/src/main/scala/package.scala
@@ -1,9 +1,32 @@
 package .stats
 
+import zd.proto.api.{N, MessageCodec}
+import zd.proto.macrosapi.{caseCodecAuto, sealedTraitCodecAuto}
+
 package object client {
-  sealed trait Stat
-  final case class MetricStat(name: String, value: String) extends Stat
-  final case class MeasureStat(name: String, value: Long) extends Stat
-  final case class ErrorStat(exception: String, stacktrace: String, toptrace: String) extends Stat
-  final case class ActionStat(action: String) extends Stat
+  sealed trait ClientMsg
+  @N(1) final case class MetricMsg
+    ( @N(1) name: String
+    , @N(2) value: String
+    , @N(3) port: String) extends ClientMsg
+  @N(2) final case class MeasureMsg
+    ( @N(1) name: String
+    , @N(2) value: String
+    , @N(3) port: String) extends ClientMsg
+  @N(3) final case class ErrorMsg
+    ( @N(1) exception: String
+    , @N(2) stacktrace: String
+    , @N(3) toptrace: String
+    , @N(4) port: String) extends ClientMsg
+  @N(4) final case class ActionMsg
+    ( @N(1) action: String
+    , @N(2) port: String) extends ClientMsg
+
+  implicit val ClientMsgCodec: MessageCodec[ClientMsg] = {
+    implicit val MetricMsgCodec: MessageCodec[MetricMsg] = caseCodecAuto[MetricMsg]
+    implicit val MeasureMsgCodec: MessageCodec[MeasureMsg] = caseCodecAuto[MeasureMsg]
+    implicit val ErrorMsgCodec: MessageCodec[ErrorMsg] = caseCodecAuto[ErrorMsg]
+    implicit val ActionMsgCodec: MessageCodec[ActionMsg] = caseCodecAuto[ActionMsg]
+    sealedTraitCodecAuto[ClientMsg]
+  }
 }
