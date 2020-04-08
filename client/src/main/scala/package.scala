9bde388d494aa25556c608f8034165f1009bb27e
Andrey Nemchenko <andrey.nemchenko@playtech.com>
2020-04-08 19:42:51 +0300

    MCBE-1285 Identify nodes by inner IP address

--- client/src/main/scala/package.scala
+++ client/src/main/scala/package.scala
@@ -1,35 +1,31 @@
 package .stats
 
-import zd.proto.api.{N, MessageCodec}
+import zd.proto.api.{N, RestrictedN, MessageCodec}
 import zd.proto.macrosapi.{caseCodecAuto, sealedTraitCodecAuto}
 
 package object client {
   sealed trait ClientMsg
-  @N(1) final case class MetricMsg
+  @N(1) @RestrictedN(3) final case class MetricMsg
     ( @N(1) name: String
     , @N(2) value: String
-    , @N(3) port: Option[String]
     , @N(4) hostname: Option[String]
     , @N(5) ipaddr: Option[String]
     ) extends ClientMsg
-  @N(2) final case class MeasureMsg
+  @N(2) @RestrictedN(3) final case class MeasureMsg
     ( @N(1) name: String
     , @N(2) value: String
-    , @N(3) port: Option[String]
     , @N(4) hostname: Option[String]
     , @N(5) ipaddr: Option[String]
     ) extends ClientMsg
-  @N(3) final case class ErrorMsg
+  @N(3) @RestrictedN(4) final case class ErrorMsg
     ( @N(1) exception: String
     , @N(2) stacktrace: String
     , @N(3) toptrace: String
-    , @N(4) port: Option[String]
     , @N(5) hostname: Option[String]
     , @N(6) ipaddr: Option[String]
     ) extends ClientMsg
-  @N(4) final case class ActionMsg
+  @N(4) @RestrictedN(2) final case class ActionMsg
     ( @N(1) action: String
-    , @N(2) port: Option[String]
     , @N(3) hostname: Option[String]
     , @N(4) ipaddr: Option[String]
     ) extends ClientMsg
