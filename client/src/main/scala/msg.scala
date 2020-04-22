ab1fd4bc21d7e02040020444f847f8aee2feb102
Andrey Nemchenko <andrey.nemchenko@playtech.com>
2020-04-22 20:18:20 +0300

    send cpu_mem on action

---
+++ client/src/main/scala/msg.scala
@@ -0,0 +1,33 @@
+package .stats.client
+
+import zd.proto.api.{N, RestrictedN}
+
+sealed trait ClientMsg
+
+@N(1) @RestrictedN(3) final case class MetricMsg
+  ( @N(1) name: String
+  , @N(2) value: String
+  , @N(4) hostname: Option[String]
+  , @N(5) ipaddr: Option[String]
+  ) extends ClientMsg
+
+@N(2) @RestrictedN(3) final case class MeasureMsg
+  ( @N(1) name: String
+  , @N(2) value: String
+  , @N(4) hostname: Option[String]
+  , @N(5) ipaddr: Option[String]
+  ) extends ClientMsg
+
+@N(3) @RestrictedN(4) final case class ErrorMsg
+  ( @N(1) exception: String
+  , @N(2) stacktrace: String
+  , @N(3) toptrace: String
+  , @N(5) hostname: Option[String]
+  , @N(6) ipaddr: Option[String]
+  ) extends ClientMsg
+
+@N(4) @RestrictedN(2) final case class ActionMsg
+  ( @N(1) action: String
+  , @N(3) hostname: Option[String]
+  , @N(4) ipaddr: Option[String]
+  ) extends ClientMsg
