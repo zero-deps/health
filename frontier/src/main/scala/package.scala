fd89b47d206348360b8ceebc015a0db1004cf88f
Andrey Nemchenko <andrey.nemchenko@playtech.com>
2020-10-08 20:53:23 +0300

    remove frontier

--- frontier/src/main/scala/package.scala
+++
@@ -1,8 +0,0 @@
-package zero
-
-import zio._
-
-package object ws {
-  def parseDouble(v: String): IO[ParseErr, Double] = IO.fromOption(v.toDoubleOption).orElseFail(ParseErr(v))
-  def parseInt(v: String): IO[ParseErr, Int] = IO.fromOption(v.toIntOption).orElseFail(ParseErr(v))
-}
