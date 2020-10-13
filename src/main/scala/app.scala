ad47fa635b511b011c74663ea9c697fbb7792d0e
Andrii Nemchenko <a.nemchenko@icloud.com>
2020-10-13 14:40:21 +0300

    broadcast

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -2,16 +2,15 @@ package .stats
 
 import annotation.unused
 
-import zio._, nio._, core._, clock.Clock // core.channels._,
-import kvs.{Kvs=>_,Err=>_,Throwed=>_,_}
-import kvs.seq._
-import ftier._, ws._
+import zio._, nio._, core._, clock._, stream._
+import kvs.{Kvs=>_,Err=>_,Throwed=>_,_}, seq._
+import ftier._, ws._, udp._
 import com.typesafe.config.ConfigFactory
 import zd.proto._, api._
+import java.util.concurrent.TimeUnit.{MILLISECONDS => in_ms}
 
 sealed trait NewEvent
-case class UdpMsg() extends NewEvent
-// case class WsMsg() extends NewEvent
+case class HostEvent(host: String, ipaddr: String) extends NewEvent
 
 case class KvsErr(e: kvs.Err) extends ForeignErr
 
@@ -22,6 +21,8 @@ object StatsApp extends zio.App {
     def extract(xs: Bytes): EnData = api.decode[EnData](xs)
     def insert(x: EnData): Bytes = api.encodeToBytes(x)
   }
+
+  type Clients = Set[WsContextData]
   
   val httpHandler: http.Request => ZIO[Kvs, Err, http.Response] = {
     case UpgradeRequest(r) => upgrade(r)
@@ -31,34 +32,47 @@ object StatsApp extends zio.App {
     case ask: HealthAsk => ZIO.unit
   }
 
-  def wsHandler(queue: Queue[NewEvent]): Msg => ZIO[WsContext with Kvs, Err, Unit] = {
+  def wsHandler(queue: Queue[NewEvent], clients: Ref[Clients]): Msg => ZIO[WsContext with Kvs, Err, Unit] = {
     case msg: Binary =>
       for {
-          message  <- IO.effect(decode[Pull](msg.v.toArray)).mapError(Throwed)
-          _        <- msgHandler(queue)(message).catchAllCause(cause =>
-                          IO.effect(println(s"msg ${cause.failureOption} err ${cause.prettyPrint}"))
-                      ).fork.unit
+        message  <- IO.effect(decode[Pull](msg.v.toArray)).mapError(Throwed)
+        _        <- msgHandler(queue)(message).catchAllCause(cause =>
+                        IO.effect(println(s"msg ${cause.failureOption} err ${cause.prettyPrint}"))
+                    ).fork.unit
       } yield ()
-    case Open => for {
-      nodeStream <- Kvs.stream[EnData](fid(fid.Nodes()), None)
-      _ <- nodeStream.map(_._2).mapError(KvsErr)
-              .foreach(en => Ws.send(Binary(Chunk.fromArray(encode[.stats.Push](.stats.HostMsg(host=en.host, ipaddr=en.value, time=en.time))))))
-    } yield ()
-    case Close => ZIO.unit
+    case Open =>
+      for {
+        ctx   <- ZIO.access[WsContext](_.get)
+        _     <- clients.update(_ + ctx)
+        nodes <- Kvs.stream[EnData](fid(fid.Nodes()))
+        _     <- nodes.map(_._2).mapError(KvsErr).foreach(en => send(HostMsg(host=en.host, ipaddr=en.value, time=en.time)))
+      } yield ()
+    case Close =>
+      for {
+        ctx <- ZIO.access[WsContext](_.get)
+        _   <- clients.update(_ - ctx)
+      } yield ()
     case msg => Ws.close
   }
 
-  def workerLoop(q: Queue[NewEvent]): ZIO[Kvs, Err, Unit] = q.take.flatMap{
-    case UdpMsg() =>
-      val host = "todo"
-      val ipaddr = "todo"
-      val time = 0L //todo
+  def broadcast(clients: Ref[Clients], msg: Push): IO[Err, Unit] = {
+    for {
+      xs <- clients.get
+      _  <- ZStream.fromIterable(xs).foreach(ctx => send(msg).provideLayer(ZLayer.succeed(ctx)))
+    } yield ()
+  }
+
+  def workerLoop(q: Queue[NewEvent], @unused clients: Ref[Clients]): ZIO[Kvs with Clock, Err, Unit] = q.take.flatMap{
+    case x: HostEvent =>
+      import x.{host, ipaddr}
       for {
-        _ <- Kvs.put(fid(fid.Nodes()), en_id.str(host), EnData(value=ipaddr, time=time, host=host)).mapError(KvsErr)
+        time <- currentTime(in_ms)
+        _    <- Kvs.put(fid(fid.Nodes()), en_id.str(host), EnData(value=ipaddr, time=time, host=host)).mapError(KvsErr)
+        _    <- broadcast(clients, HostMsg(host=host, ipaddr=ipaddr, time=time))
       } yield ()
   }
 
-  val leveldbConf =  _root_.kvs.Kvs.LeveldbConf("rng_data")
+  val leveldbConf = _root_.kvs.Kvs.LeveldbConf("rng_data")
   val conf = _root_.kvs.Kvs.RngConf(leveldbConf=leveldbConf)
 
   type Stats = Has[client.Stats]
@@ -68,35 +82,34 @@ object StatsApp extends zio.App {
 
   val app: ZIO[ActorSystem, Any, Unit] = {
     (for {
-      ucfg <- IO.effect(ConfigFactory.load().getConfig("stats.server"))
-      uhos <- IO.effect(ucfg.getString("host"))
-      upor <- IO.effect(ucfg.getInt("port"))
+      q    <- Queue.unbounded[NewEvent]
+      clients <- Ref.make(Set.empty: Clients)
+      ucfg <- IO.effect(ConfigFactory.load).orDie
+      uhos <- IO.effect(ucfg.getString("stats.server.host")).orDie
+      upor <- IO.effect(ucfg.getInt   ("stats.server.port")).orDie
       udpa <- SocketAddress.inetSocketAddress(uhos, upor)
       _    <- udp.bind(udpa)(channel =>
                 (for {
                   data <- channel.read
                   msg  <- IO.effect(decode[client.ClientMsg](data.toArray))
-                  time <- IO.succeed(System.currentTimeMillis) //todo: Clock.live
-                  _    <- Kvs.put(fid(fid.Nodes()), en_id.str(msg.host), EnData(value=msg.ipaddr, time=time, host=msg.host))
-                  //todo: send to websocket
+                  _    <- q offer HostEvent(host=msg.host, ipaddr=msg.host)
                   _    <- msg match {
-                    case x: client.MetricMsg  => ZIO.unit.map(_ => println(x))
-                    case x: client.MeasureMsg => ZIO.unit.map(_ => println(x))
-                    case x: client.ErrorMsg   => ZIO.unit.map(_ => println(x))
-                    case x: client.ActionMsg  => ZIO.unit.map(_ => println(x))
+                    case x: client.MetricMsg  => ZIO.unit.map(_ => println(x)) //todo: Console.live
+                    case x: client.MeasureMsg => ZIO.unit.map(_ => println(x)) //todo: Console.live
+                    case x: client.ErrorMsg   => ZIO.unit.map(_ => println(x)) //todo: Console.live
+                    case x: client.ActionMsg  => ZIO.unit.map(_ => println(x)) //todo: Console.live
                   }
                 } yield ()).catchAll(ex => ZIO.unit.map(_ => println(ex)))
               ).use(ch => ZIO.never.ensuring(ch.close.ignore)).fork
-      q    <- Queue.unbounded[NewEvent]
-      _    <- workerLoop(q).forever.fork
+      _    <- workerLoop(q, clients).forever.fork
       addr <- SocketAddress.inetSocketAddress(8001)
       kvs  <- ZIO.access[Kvs](_.get)
       _    <- httpServer.bind(
                 addr,
                 IO.succeed(req => httpHandler(req).provideLayer(ZLayer.succeed(kvs))),
-                IO.succeed(msg => wsHandler(q)(msg).provideSomeLayer[WsContext](ZLayer.succeed(kvs)))
+                IO.succeed(msg => wsHandler(q, clients)(msg).provideSomeLayer[WsContext](ZLayer.succeed(kvs)))
               )
-    } yield ()).provideLayer(stats(leveldbConf) ++ Kvs.live(conf) ++ (Clock.live >>> udp.live(128))) //todo: get size from client conf + warn about size exceed
+    } yield ()).provideLayer(Clock.live ++ stats(leveldbConf) ++ Kvs.live(conf) ++ Udp.live(client.conf.msgSize))
   }
 
   def run(@unused args: List[String]): URIO[ZEnv, ExitCode] = {
